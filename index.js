484041978f862b7fb889d6063bf49e318ab1e7ad
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2025-09-10 08:14:52 +0600

    Change the root route possition

--- index.js
+++ index.js
@@ -5,33 +5,29 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 
-
 //MIDDLEWARES
 app.use(express.json());
 app.use(cors());
 
-// ROUTES IMPORT HERE 
-const classRoutes = require("./src/routes/classShedule.js")
-const badgetTracker = require("./src/routes/budgetTracker.js")
-const studyPlanner = require("./src/routes/studyPlanner.js")
-const generatedQuestion = require("./src/routes/generatedQuestion.js")
+// ROUTES IMPORT HERE
+const classRoutes = require("./src/routes/classShedule.js");
+const badgetTracker = require("./src/routes/budgetTracker.js");
+const studyPlanner = require("./src/routes/studyPlanner.js");
+const generatedQuestion = require("./src/routes/generatedQuestion.js");
 
 //ROUTES USE HERE
 app.use("/api/class", classRoutes);
-app.use("/api/badget-tracker", badgetTracker)
-app.use("/api/study-planner", studyPlanner)
-app.use("/api/generated-question", generatedQuestion)
+app.use("/api/badget-tracker", badgetTracker);
+app.use("/api/study-planner", studyPlanner);
+app.use("/api/generated-question", generatedQuestion);
 
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //MONGOSE CONFIGURATION IN HERE
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI);
-
-
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
 }
 
 main()
